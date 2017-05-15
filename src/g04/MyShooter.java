fa50533d292b7dcbff72d29625a65e3898427579
Weiqifan1 <cmlykke@hotmail.com>
2017-05-15 10:23:07 +0200

    MyShooter copy e2

--- src/g04/MyShooter.java
+++ src/g04/MyShooter.java
@@ -9,54 +9,96 @@ import battleship.interfaces.BattleshipsPlayer;
 import battleship.interfaces.Board;
 import battleship.interfaces.Fleet;
 import battleship.interfaces.Position;
+import battleship.interfaces.Ship;
+import java.util.Random;
 
 /**
  *
  * @author Christian
  */
 public class MyShooter implements BattleshipsPlayer {
-
+    
+    private final static Random rnd = new Random();
+    private int sizeX;
+    private int sizeY;
+    
+    private int nextX;
+    private int nextY;
+    
     public MyShooter() {
     }
 
     @Override
     public void startMatch(int rounds, Fleet ships, int sizeX, int sizeY) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //do ntohing
     }
 
     @Override
     public void startRound(int round) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //Do nothing
     }
 
     @Override
     public void placeShips(Fleet fleet, Board board) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        nextX = 0;
+        nextY = 0;
+        sizeX = board.sizeX();
+        sizeY = board.sizeY();
+        for(int i = 0; i < fleet.getNumberOfShips(); ++i)
+        {
+            Ship s = fleet.getShip(i);
+            boolean vertical = rnd.nextBoolean();
+            Position pos;
+            if(vertical)
+            {
+                int x = rnd.nextInt(sizeX);
+                int y = rnd.nextInt(sizeY-(s.size()-1));
+                pos = new Position(x, y);
+            }
+            else
+            {
+                int x = rnd.nextInt(sizeX-(s.size()-1));
+                int y = rnd.nextInt(sizeY);
+                pos = new Position(x, y);
+            }
+            board.placeShip(pos, s, vertical);
+        }
     }
 
     @Override
     public void incoming(Position pos) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //Do nothing
     }
 
     @Override
     public Position getFireCoordinates(Fleet enemyShips) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Position shot = new Position(nextX, nextY);
+        ++nextX;
+        if(nextX >= sizeX)
+        {
+            nextX = 0; 
+            ++nextY;
+            if(nextY >= sizeY)
+            {
+                nextY = 0;
+            }
+        }
+        return shot;
     }
 
     @Override
     public void hitFeedBack(boolean hit, Fleet enemyShips) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //Do nothing
     }
 
     @Override
     public void endRound(int round, int points, int enemyPoints) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //Do nothing
     }
 
     @Override
     public void endMatch(int won, int lost, int draw) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //Do nothing
     }
     
 }
